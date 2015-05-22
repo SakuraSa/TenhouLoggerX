5d306590589bdcbd29bfabe930c01e345c15af64
SakuraSa <sakura_kyon@hotmail.com>
2015-05-22 18:15:26 +0800

    add xheaders cmdline options

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ if __name__ == '__main__':
 
     define("port", default=80, help="run on the given port", type=int)
     define("ip", default="0.0.0.0", help="binding on the given ip", type=str)
+    define("xheaders", default=False, help="get real ip from X-Read-Ip headers", type=bool)
 
     tornado.options.parse_command_line()
     app = create_app()
-    app.listen(options.port, options.ip)
+    app.listen(port=options.port, address=options.ip, xheaders=options.xheaders)
     print "starting service on http://%s:%s" % (options.ip, options.port)
     tornado.ioloop.IOLoop.instance().start()
